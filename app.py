1fe5722ee6687999e9f1044f2a91a8745c69df15
Carlos16897 <carlosdeiviroce@gmail.com>
2025-07-30 13:04:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,1 @@
-def saludo(nombre):
-    print("Hola " + nombre)
+def saludo(nombre): print("Hola " + nombre + ", bienvenido a este programa que tiene una línea excesivamente larga para que flake8 la detecte")
